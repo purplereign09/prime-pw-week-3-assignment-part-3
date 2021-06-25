e259580bb46b822b8c446eba32f1795729d5bc9e
Patty Baraibar <pattybaraibar@msn.com>
2021-06-25 01:03:26 -0500

    first commit

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -1,35 +1,58 @@
 console.log('****** Part Supply *******');
 // REQUIRED FEATURES
 
-// 1. Create a variable called 'partsNeeded' with a value of the number 40 
+// 1. Create a variable called 'partsNeeded' with a value of the number 40
 //    & console.log the variable
-console.log('1. Number of partsNeeded:');
+let partsNeeded = 40;
+console.log('1. Number of partsNeeded:', partsNeeded);
+
+//console.log(partsNeeded);
 
 // 2. Create a variable call 'supplyChanges' set it to an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11
 console.log('2. Array of supplyChanges:');
+let supplyChanges = [ 3, 5, -6, 0, 7, 11];
 
 // 3. Console log the value of 'supplyChanges' at index 2
-console.log('3. Item at index 2 is:');
+console.log('3. Item at index 2 is:', supplyChanges[2]);
+//console.log(supplyChanges[2]);
 
-// 4. The last item was added by mistake. Remove it from the 'supplyChanges' 
+// 4. The last item was added by mistake. Remove it from the 'supplyChanges'
 //    array & console.log the value removed.
-console.log('4. Removed item:');
+supplyChanges.pop();
+let x = supplyChanges.pop();
+//console.log(x);
+console.log('4. Removed item:', x);
+
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
-console.log('5. Adding 25 to supplyChanges.');
+let i = supplyChanges.push(25);
+console.log(supplyChanges);
+
 
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
-//    - if it is a positive number (greater than 0), log 'Added x parts.' 
+//    - if it is a positive number (greater than 0), log 'Added x parts.'
 //    - if the value is 0, log 'No Change.'
-//    - if the value is negative, format the log as 'Removed x parts.' 
-console.log('6. Showing supplyChanges...');
+//    - if the value is negative, format the log as 'Removed x parts.'
+x = 3;
+
+for(x > 0) {
+  console.log('Added x parts.');
+}
+else if(x = 0) {
+  console.log('No Change.');
+}
+else(x < 0) {
+  console.log('Removed x ');
+}
+
+//console.log('6. Showing supplyChanges...');
 
 
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
-// 7. Rewrite the `for` loop from #6 as a `for of` loop. 
+// 7. Rewrite the `for` loop from #6 as a `for of` loop.
 console.log('7. Showing supplyChanges with "for of" loop');
 
 // 8. Rewrite the `for` loop from #6 as a `while` loop.
